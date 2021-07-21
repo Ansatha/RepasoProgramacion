24d22f219c82e74c351c54b0ab5367a8bc5b0fe0
ansatha <ansatha@hotmail.com>
2021-07-21 14:43:21 +0200

    ajustes pre correccion

--- 11/RepasoWhile.ts
+++ 11/RepasoWhile.ts
@@ -30,6 +30,9 @@ function fechaToZodiaco(fecha:Date):string
     };  
     return signos[index];
 };
+/*                         ↑↑↑  FUNCTION  ↑↑↑
+-------------------------------------------------------------------------------------
+                           ↓↓↓    TEST    ↓↓↓                                      */
 
 let fecha0:string = "January 20";
 let fecha:Date = new Date(fecha0);
@@ -42,9 +45,55 @@ console.log('\nSi has nacido en la fecha:', fecha1, 'eres', fechaToZodiaco(fecha
 //FIN PUNTO 1.=======================================================================
 
 //PUNTO 2.===========================================================================
+function continentFromCountry(countryIn:string):void
+{   
+    let whatContinent:string;
+    const continentsCountry = {
+        'Europa' : ['España', 'Portugal', 'Holanda', 'Suiza', 'Rumania'],
+        'Africa' : ['Marruecos', 'Egipto', 'Sudafrica', 'Zaire', 'Congo'],
+        'Asia'   : ['Japon', 'China', 'India', 'Tailandia', 'Vietnam'],
+        'America': ['EEUU', 'Argentina', 'Brasil', 'Chile', 'Cuba'],
+        'Oceania': ['Australia', 'Fiji', 'Samoa', 'Kiribati', 'Tonga']
+    };
+    let continents:string[] = ['Europa', 'Africa', 'Asia', 'America', 'Oceania'];
 
+    let indexContinents:number = 0;
+    let continentFound:boolean = false;
+    let checkCountry:string;
+    while (indexContinents < continents.length && continentFound == false)
+    {   
+        let indexCountry:number = 0;
+        while (indexCountry < continentsCountry[continents[indexContinents]].length && continentFound == false)
+        {
+            whatContinent = continents[indexContinents];
+            checkCountry = continentsCountry[whatContinent][indexCountry];
+            continentFound = (checkCountry === countryIn ? true : false);
+            indexCountry++;   
+        };
+        indexContinents++;
+    };
+    continentFound ? console.log('El país', countryIn, 'está en', whatContinent + '.\n') :
+    console.log('\n\x1b[31m%s\x1b[0m','Error:', '\n\'' + countryIn + '\' no está incluido en las listas.\nElige otro país.\n');
+};
+
+/*                         ↑↑↑  FUNCTION  ↑↑↑
+-------------------------------------------------------------------------------------
+                           ↓↓↓    TEST    ↓↓↓                                      */
+
+let country1:string = "España";
+console.log('Ejemplo 1:');
+continentFromCountry(country1);
+
+let country2:string = "Toga";
+console.log('Ejemplo 2:');
+continentFromCountry(country2);
 //FIN PUNTO 2.=======================================================================
 
-//PUNTO 2.===========================================================================
 
-//FIN PUNTO 2.=======================================================================
+
+//PUNTO 3.===========================================================================
+/*                         ↑↑↑  FUNCTION  ↑↑↑
+-------------------------------------------------------------------------------------
+                           ↓↓↓    TEST    ↓↓↓                                      */
+
+//FIN PUNTO 3.=======================================================================
