230028a477856a8ca13649f39d921b81ec91bfdd
ansatha <ansatha@hotmail.com>
2021-07-21 16:03:03 +0200

    retos listos para corregir

--- 10/10.js
+++ 10/10.js
@@ -1,11 +1,29 @@
-"use strict";
-exports.__esModule = true;
-var sumLettersFromArray_1 = require("../08/sumLettersFromArray");
-var oddVsEven_1 = require("../09/oddVsEven");
+//PUNTO 10.===========================================================================
+function oddsVsEvens(numIn) {
+    console.log('El numero', numIn, numIn % 2 === 0 ? 'es par' : 'es impar');
+}
+;
+function sumLettersFromArray(arrayIn) {
+    var sum = 0;
+    for (var word in arrayIn) {
+        sum += arrayIn[word].length;
+    }
+    ;
+    return sum;
+}
+;
+/*                         ↑↑↑  FUNCTIONS  ↑↑↑
+-------------------------------------------------------------------------------------
+                           ↓↓↓    TEST    ↓↓↓                                      */
 var cNames = ['Casa', 'Coche', 'Ciudad', 'Cesta'];
 var bNames = ['Barco', 'Baca', 'Bicicleta', 'Balon', 'Bisiesto', 'Brasil'];
 var vNames = ['Venezuela', 'Veneno', 'Voltaje'];
-oddVsEven_1.oddVsEven(sumLettersFromArray_1.sumLettersFromArray(cNames));
-oddVsEven_1.oddVsEven(sumLettersFromArray_1.sumLettersFromArray(bNames));
-oddVsEven_1.oddVsEven(sumLettersFromArray_1.sumLettersFromArray(vNames));
+console.log('\n\n\x1b[35m%s\x1b[0m', '10º RETO TEST.');
+console.log('\x1b[36m%s\x1b[0m', 'Ejemplo 1:');
+oddsVsEvens(sumLettersFromArray(cNames));
+console.log('\n\x1b[36m%s\x1b[0m', 'Ejemplo 2:');
+oddsVsEvens(sumLettersFromArray(bNames));
+console.log('\n\x1b[36m%s\x1b[0m', 'Ejemplo 3:');
+oddsVsEvens(sumLettersFromArray(vNames));
 console.log();
+//FIN PUNTO 10.=======================================================================
--- 10/10.ts
+++ 10/10.ts
@@ -1,11 +1,31 @@
-import { sumLettersFromArray } from '../08/sumLettersFromArray';
-import { oddVsEven } from '../09/oddVsEven';
+//PUNTO 10.===========================================================================
+function oddsVsEvens(numIn:number):void
+{
+    console.log('El numero', numIn, numIn % 2 === 0 ? 'es par' : 'es impar');
+};
+function sumLettersFromArray(arrayIn:string[]):number
+{
+    let sum:number = 0;
+    for(let word in arrayIn)
+    {   
+        sum += arrayIn[word].length;
+    };
+    return sum
+};
+/*                         ↑↑↑  FUNCTIONS  ↑↑↑
+-------------------------------------------------------------------------------------
+                           ↓↓↓    TEST    ↓↓↓                                      */
 
 const cNames:string[] = ['Casa', 'Coche', 'Ciudad', 'Cesta'];
 const bNames:string[] = ['Barco', 'Baca', 'Bicicleta', 'Balon', 'Bisiesto', 'Brasil'];
 const vNames:string[] = ['Venezuela', 'Veneno', 'Voltaje'];
 
-oddVsEven(sumLettersFromArray(cNames));
-oddVsEven(sumLettersFromArray(bNames));
-oddVsEven(sumLettersFromArray(vNames));
-console.log();
+console.log('\n\n\x1b[35m%s\x1b[0m','10º RETO TEST.')
+console.log('\x1b[36m%s\x1b[0m','Ejemplo 1:');
+oddsVsEvens(sumLettersFromArray(cNames));
+console.log('\n\x1b[36m%s\x1b[0m','Ejemplo 2:');
+oddsVsEvens(sumLettersFromArray(bNames));
+console.log('\n\x1b[36m%s\x1b[0m','Ejemplo 3:');
+oddsVsEvens(sumLettersFromArray(vNames));
+console.log();                          
+//FIN PUNTO 10.=======================================================================
